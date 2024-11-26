0ad41c6ae0a809f2ea07a02e699f5629940a067a
412-ChrisP <chrispbacon542@gmail.com>
2024-11-26 09:35:38 -0500

    finished table 3 inventory

--- Dealership Database Script.sql
+++ Dealership Database Script.sql
@@ -2,16 +2,18 @@ DROP DATABASE IF EXISTS CarDealership;
 CREATE DATABASE CarDealership;
 USE CarDealership;
 
--- Table 1: dealerships
-CREATE TABLE dealerships (
+-- Table 1) dealerships
+CREATE TABLE dealerships 
+(
     dealership_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     address VARCHAR(50),
     phone VARCHAR(12)
 );
 
--- Table 2: vehicles
-CREATE TABLE vehicles (
+-- Table 2) vehicles
+CREATE TABLE vehicles 
+(
     vin INT PRIMARY KEY,
     year INT NOT NULL,
     make VARCHAR(50) NOT NULL,
@@ -21,5 +23,12 @@ CREATE TABLE vehicles (
     odometer INT,
     price DOUBLE NOT NULL,
     sold BOOLEAN DEFAULT FALSE
+);
+
+-- Table 3) inventory
+CREATE TABLE inventory 
+(
+    dealership_id INT,
+    vin INT
 );
 
