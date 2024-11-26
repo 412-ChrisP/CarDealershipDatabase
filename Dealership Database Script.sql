ca53814f1ce744043f16db1c8ce5d164df1633d0
412-ChrisP <chrispbacon542@gmail.com>
2024-11-26 09:01:59 -0500

    Finished table 1 dealerships

--- Dealership Database Script.sql
+++ Dealership Database Script.sql
@@ -2,3 +2,11 @@ DROP DATABASE IF EXISTS CarDealership;
 CREATE DATABASE CarDealership;
 USE CarDealership;
 
+-- Table 1: dealerships
+CREATE TABLE dealerships (
+    dealership_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    address VARCHAR(50),
+    phone VARCHAR(12)
+);
+
